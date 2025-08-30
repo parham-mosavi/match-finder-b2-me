4cd17798f1fa026ada6fa7bf9c5eb27ab3e6fbdd
parham-mosavi <pa12.ha.34.o@gmail.com>
2025-08-30 23:51:56 +0330

    router-outlet

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e885b5b3b0ece53fa81b2e90556bc66ebb39fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ee19ee9b36dc74ea6f50c3394a315176f1238d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
