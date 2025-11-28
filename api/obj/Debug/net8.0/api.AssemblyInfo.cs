ad0f08c61b95ef819d559f74f6a083cd20324543
parham-mosavi <pa12.ha.34.o@gmail.com>
2025-11-29 01:20:08 +0330

    finish guards

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc48ce77694f641afa4a1f85280465a5d154f49c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551c91d7757105461205d96776ee1378a49e4be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,15 +7,18 @@ import { RegisterComponent } from './components/account/register/register.compon
 import { HomeComponent } from './components/home/home.component';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { MemberCardComponent } from './components/members/member-card/member-card.component';
+import { authGuard } from './guards/auth.guard';
+import { authLoggedInGuard } from './guards/auth-logged-in.guard';
 
 export const routes: Routes = [{
-     path: '', component: HomeComponent },
+    path: '', component: HomeComponent
+},
 // { path: 'home', component: HomeComponent },
-    { path: 'account/register', component: RegisterComponent },
-    { path: 'account/login', component: LoginComponent },
-    { path: 'footer', component: FooterComponent },
-    { path: 'navbar', component: NavbarComponent },
-    { path: 'members/member-list', component: MemberListComponent },
-    { path: 'members/member-card', component: MemberCardComponent },
-    { path: '**', component: NotFoundComponent }
+{ path: 'account/register', component: RegisterComponent, canActivate: [authLoggedInGuard] },
+{ path: 'account/login', component: LoginComponent, canActivate: [authLoggedInGuard] },
+{ path: 'footer', component: FooterComponent },
+{ path: 'navbar', component: NavbarComponent },
+{ path: 'members/member-list', component: MemberListComponent, canDeactivate: [authGuard] },
+{ path: 'members/member-card', component: MemberCardComponent },
+{ path: '**', component: NotFoundComponent }
 ];
